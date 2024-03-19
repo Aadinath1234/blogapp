fba1bdebe4565ac6177a017a156cde571a5eb1e7
Aadinath1234 <aadeshsatpute375@gmail.com>
2024-03-19 23:20:26 +0530

    client files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/account/Login'
+
+function App() {
+  return (
+    <div style={{marginTop: 64}}>
+     <Login />
+     </div>
+     
+        
+  );
+}
+
+export default App;
+
